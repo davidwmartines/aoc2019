1423df0579be9378c791eabfbf113e64e1709223
David W Martines <d5172@yahoo.com>
2019-12-01 09:14:05 -0600

    solve day1a

--- day1a/main.go
+++ day1a/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hello!")
+	file, err := os.Open("./day1a/input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+		fuel := calculateFuel(mass)
+		total += fuel
+	}
+	fmt.Printf("total: %v\n", total)
 }
 
 func calculateFuel(mass int) int {
